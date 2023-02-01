69d21a169fb180bc6aa14e37e591cadb0c270a21
Mike McGrath <mmcgrath@drift.com>
2023-02-01 18:24:30 -0500

    push

--- pages/api/keywords.ts
+++ pages/api/keywords.ts
@@ -1,23 +1,29 @@
 import { NextApiRequest } from "next";
 
+const axios = require("axios");
+const cheerio = require("cheerio");
 const { Configuration, OpenAIApi } = require("openai");
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req: NextApiRequest, res: any) {
-  const { text } = req.body;
+  const { url } = req.body;
 
   try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    const body = $("p").text();
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: text,
-      temperature: 0.7,
+      prompt: `Extract keywords from this text:\n\n${body}`,
+      temperature: 0.5,
       max_tokens: 60,
       top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 1,
+      frequency_penalty: 0.8,
+      presence_penalty: 0,
     });
 
     res.status(200).json(response.data);
